741b5b794a00641f65914a2d3fc0091e03d12e99
Андрей Беляков <ttxiom@gmail.com>
2022-02-06 23:39:18 +0500

    up

---
+++ tasks/task15/16-1.py
@@ -0,0 +1,29 @@
+# https://inf-ege.sdamgia.ru/problem?id=7929
+''' Элементами множеств А, P, Q являются натуральные числа, причём 
+P = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20}, 
+Q = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30}. 
+    Известно, что выражение
+( (x ∈ A) → (x ∈ P) ) ∧ ( (x ∈ Q) → ¬(x ∈ A) )
+истинно при любом значении переменной х. 
+Определите наибольшее возможное количество элементов в множестве A.'''
+
+from functools import reduce
+
+p = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+q = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30}
+z = p.union(q)
+
+lst = reduce(lambda P, x: P + [subset | {x} for subset in P], z, [set()])
+
+d = 0
+for a in lst:
+    for x in z:
+        f1 = (x in a) <= (x in p)
+        f2 = (x in q) <= (not(x in a))
+        f = f1 and f2
+        if not f:
+            break
+    else:
+        d = max(d,len(a))
+
+print(d)
