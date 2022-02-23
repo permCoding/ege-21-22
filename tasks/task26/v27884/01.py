d1f0a2a8383b93ed99442f3f946ab8cec2eb78e2
Андрей Беляков <ttxiom@gmail.com>
2022-02-23 08:08:17 +0500

    up

---
+++ tasks/task26/v27884/01.py
@@ -0,0 +1,20 @@
+with open("./27884.txt") as f:
+    lines = f.readlines()
+
+s, n = map(int, lines[0].split())
+nums = sorted(map(int, lines[1:]))
+
+sm = 0
+for pos in range(0, n):
+    if sm + nums[pos] <= s:
+        sm += nums[pos]
+    else:
+        break
+
+print(pos)
+check = s - sm + nums[pos-1]
+
+for end in range(n-1, pos-1, -1):
+    if nums[end] <= check:
+        print(nums[end])
+        break
