a82d665b69ed3577071b4ec8ac80ef8beb17d9fd
Андрей Беляков <ttxiom@gmail.com>
2022-02-25 17:55:48 +0500

    gen

--- tasks/task15/99-gen-3.py
+++ tasks/task15/99-gen-3.py
@@ -5,15 +5,14 @@
 
 lst = [1, 3, 4]
 
-subsets = []
-
 n = len(lst)
-count = 2 ** n
-for i in range(count):
+subsets = []
+for i in range(2 ** n):
     subset = []
+    bin_num = bin(i)[2:].rjust(3, '0')
+    # print(bin_num)
     for j in range(n):
-        mask = 1 << j
-        if mask & i > 0:
+        if bin_num[j] == '1':
             subset += [lst[j]]
     subsets.append(subset)
 
