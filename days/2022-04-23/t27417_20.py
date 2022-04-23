ea59ab8b7441bab77df27eb6aa521e65378be7f1
permcoding <ttxiom@gmail.com>
2022-04-23 12:46:59 +0500

    task_21

--- days/2022-04-23/t27417_20.py
+++ days/2022-04-23/t27417_20.py
@@ -8,14 +8,14 @@
 # 0 1 2 3 4
 # 0 П В П В
 def f20(a,b,p):
-    if p==2: return a+b>=77
-    # if p==1 and a+b>=77: return False
+    if p==3: return a+b>=77
+    if p<3 and a+b>=77: return False
     p += 1
     b1, b2 = f20(a+1,b,p), f20(a,b+1,p)
     b3, b4 = f20(a*2,b,p), f20(a,b*2,p)
+    if p==2: return b1 and b2 and b3 and b4
     return b1 or b2 or b3 or b4
 
 for s in range(1, 70):
     if f20(7,s,0):
         print(s)
-        break
